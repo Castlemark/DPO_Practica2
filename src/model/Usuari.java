9951746c71623d5a21485f0f19a796b0fefc1f71
Propietario <pau.nonell@gmail.com>
2017-03-30 08:34:16 +0200

    fgvhjnklñ

--- src/model/Usuari.java
+++ src/model/Usuari.java
@@ -7,4 +7,20 @@ public class Usuari {
     private String login;
     private String mail;
     private String password;
+
+    private boolean comprovaDades(String nomUsuari, String correu, String contrasenya, String confirmacioContra){
+        return true;
+    }
+
+    private boolean registreUsuari(String nomUsuari, String correu, String contrasenya){
+        return true;
+    }
+
+    private boolean iniciarSessio(String nomUsuariCorreu, String contrasenya){
+        return true;
+    }
+
+    private boolean tancarSessio(){
+        return true;
+    }
 }
