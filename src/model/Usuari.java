d865491863dc0075812a51487ed094444de14178
Castlemark <marc.castells.guell@gmail.com>
2017-03-30 08:36:47 +0200

    holi

--- src/model/Usuari.java
+++ src/model/Usuari.java
@@ -8,6 +8,37 @@ public class Usuari {
     private String mail;
     private String password;
 
+    public Usuari (){
+
+    }
+    public Usuari(String login, String mail, String password){
+
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     private boolean comprovaDades(String nomUsuari, String correu, String contrasenya, String confirmacioContra){
         return true;
     }
