08da9f767b3de6d05cb9d4ca87b5589740fbe174
Tatiana <tatiana.caceres.2015@salleurl.edu>
2017-05-01 20:39:04 +0200

    Vistes  client

--- src/Vista/IniciarSessio.java
+++ src/Vista/IniciarSessio.java
@@ -8,7 +8,6 @@ import java.awt.*;
  */
 public class IniciarSessio extends JPanel {
     private JPanel jpIniciar;
-    private JPanel jpAtras;
     private JButton jbAtras;
     private JLabel jlTitle;
     private JLabel jlID;
@@ -20,16 +19,12 @@ public class IniciarSessio extends JPanel {
     private JButton jbIniciar;
 
     public IniciarSessio () {
-        this.setTitle ("Iniciar Sessió");
         this.setSize (350,350);
-        this.setResizable(true);
 
         jpIniciar = new JPanel();
-        jpAtras = new JPanel(new BorderLayout());
         jpID = new JPanel(new BorderLayout());
         jpPassword = new JPanel(new BorderLayout());
 
-        jbAtras = new JButton("<-");
         jlTitle = new JLabel("            Iniciar Sessió");
         jlID = new JLabel("ID         ");
         jtID = new JTextField();
@@ -39,14 +34,12 @@ public class IniciarSessio extends JPanel {
 
 
         jpIniciar.setLayout(new GridLayout(4,1));
-        jpAtras.add (jbAtras, BorderLayout.LINE_START);
-        jpAtras.add (jlTitle, BorderLayout.CENTER);
         jpID.add(jlID, BorderLayout.LINE_START);
         jpID.add(jtID, BorderLayout.CENTER);
         jpPassword.add (jlPassword, BorderLayout.LINE_START);
         jpPassword.add (jtPassword, BorderLayout.CENTER);
 
-        jpIniciar.add(jpAtras, BorderLayout.CENTER);
+        jpIniciar.add(jlTitle, BorderLayout.CENTER);
         jpIniciar.add(jpID);
         jpIniciar.add(jpPassword);
         jpIniciar.add(jbIniciar, BorderLayout.LINE_END);
