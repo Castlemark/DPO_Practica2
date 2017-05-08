6ce94404ce23038fd45e828c012f4eee1f1110d0
Tatiana <tatiana.caceres.2015@salleurl.edu>
2017-05-08 17:25:02 +0200

    Vistes  client proves

---
+++ src/Vista/FiPartida.java
@@ -0,0 +1,7 @@
+package Vista;
+
+/**
+ * Created by sullivan on 08/05/2017.
+ */
+public class FiPartida {
+}
--- src/Vista/Prova4.java
+++ src/Vista/Prova4.java
@@ -1,5 +1,5 @@
 /*
- * Created by JFormDesigner on Wed May 03 09:09:49 CEST 2017
+ * Created by JFormDesigner on Mon May 08 17:15:52 CEST 2017
  */
 
 package Vista;
@@ -10,17 +10,19 @@ import javax.swing.*;
 /**
  * @author Tatiana CÃ¡ceres
  */
-public class Proba2 extends JPanel {
-    public Proba2() {
+public class Prova4 extends JPanel {
+    public Prova4() {
         initComponents();
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Tatiana CÃ¡ceres
-        label2 = new JLabel();
+        label3 = new JLabel();
         label1 = new JLabel();
+        label2 = new JLabel();
         panel1 = new JPanel();
+        panel2 = new JPanel();
 
         //======== this ========
 
@@ -32,23 +34,30 @@ public class Proba2 extends JPanel {
                 java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
         setLayout(new GridBagLayout());
-        ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0, 0};
-        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0};
-        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
-        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
 
-        //---- label2 ----
-        label2.setText(" ");
-        add(label2, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+        //---- label3 ----
+        label3.setText(" ");
+        add(label3, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-            new Insets(0, 0, 53, 73), 0, 0));
+            new Insets(0, 0, 25, 25), 0, 0));
 
         //---- label1 ----
-        label1.setText("LSTRONER WELCOME!");
-        label1.setFont(new Font("Akrobat ExtraBold", Font.PLAIN, 26));
-        add(label1, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+        label1.setText("Registra't");
+        label1.setFont(new Font("Akrobat Bold", Font.PLAIN, 24));
+        add(label1, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-            new Insets(0, 0, 53, 73), 0, 0));
+            new Insets(0, 0, 25, 25), 0, 0));
+
+        //---- label2 ----
+        label2.setText("Inicia Sessi\u00f3");
+        label2.setFont(new Font("Akrobat Bold", Font.PLAIN, 22));
+        add(label2, new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 25, 25), 0, 0));
 
         //======== panel1 ========
         {
@@ -58,16 +67,30 @@ public class Proba2 extends JPanel {
             ((GridBagLayout)panel1.getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
             ((GridBagLayout)panel1.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
         }
-        add(panel1, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0,
+        add(panel1, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-            new Insets(0, 0, 53, 73), 0, 0));
+            new Insets(0, 0, 25, 25), 0, 0));
+
+        //======== panel2 ========
+        {
+            panel2.setLayout(new GridBagLayout());
+            ((GridBagLayout)panel2.getLayout()).columnWidths = new int[] {0, 0, 0};
+            ((GridBagLayout)panel2.getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+            ((GridBagLayout)panel2.getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
+            ((GridBagLayout)panel2.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+        }
+        add(panel2, new GridBagConstraints(3, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 25, 25), 0, 0));
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Tatiana CÃ¡ceres
-    private JLabel label2;
+    private JLabel label3;
     private JLabel label1;
+    private JLabel label2;
     private JPanel panel1;
+    private JPanel panel2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
